d2dd59a5ee77a016bcc9b15c714c267f009c9cdc
Arkolips <catel.ludovic@gmail.com>
2016-10-22 02:17:59 +0200

    continue the structure of project

--- index.php
+++ index.php
@@ -2,9 +2,24 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Bienvenue sur mon site - Arkoroom</title>
+    <meta name="description" content="Développeur frontend, développeur junior, intégrateur Web, étudiant Webforce3">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta name="keywords" content="développement, développement web, intégration web">
+    <meta name="author" content="Ludovic Catel">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Bienvenue sur mon site - CATEL Ludovic</title>
+    <link rel="stylesheet" href="private/vendor/materialize/dist/css/materialize.min.css" media="screen" title="no title">
+    <link rel="stylesheet" href="private/vendor/font-awesome/css/font-awesome.min.css" media="screen">
+    <link href="https://fonts.googleapis.com/css?family=Droid+Sans|Oswald" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/style.css" media="screen" title="no title">
+    <link rel="icon" type="image/png" href="public/img/cherry.jpg" />
   </head>
   <body>
     <h1>CATEL Ludovic</h1>
+
+    <script src="private/vendor/jquery/dist/jquery.min.js" charset="utf-8"></script>
+    <script src="private/vendor/materialize/dist/js/materialize.min.js" charset="utf-8"></script>
+    <script src="public/js/script.js" charset="utf-8"></script>
   </body>
 </html>
